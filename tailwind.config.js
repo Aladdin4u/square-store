2137d83286db8281cfa54841d04c0c047cc3fe15
aladdin4u <taofikakinkumi@gmail.com>
2023-05-20 03:29:05 +0100

    sign in pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,19 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-       gridTemplateRows: {
-          '[auto,auto,1fr]': 'auto auto 1fr',
-        },
+      gridTemplateRows: {
+        "[auto,auto,1fr]": "auto auto 1fr",
+      },
     },
   },
-  plugins: [require('@tailwindcss/aspect-ratio'),],
-}
-
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
+};
